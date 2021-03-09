56b2f47b3eb2add016e7afe02b6b7689e6e8246c
Nils Borg <Nils.Borg@abbindustrigymnasium.se>
2021-03-09 08:54:30 +0100

    Update find_lights.py

--- vision/find_lights.py
+++ vision/find_lights.py
@@ -1,25 +1,33 @@
 # Untitled - By: s8jesjan - Tue Aug 18 2020
-
+from machine import I2C
+from fpioa_manager import fm
+from board import board_info
 import sensor
 import image
 import lcd
 import time
 
+fm.register(board_info.PIN15, fm.fpioa.UART1_TX, force=True)
+fm.register(board_info.PIN17, fm.fpioa.UART1_RX, force=True)
+
+uart_A = UART(UART.UART1, 9600, 8, None, 1, timeout=2000, read_buf_len=4096)
 lcd.init(freq=15000000)
-sensor.reset()
+sensor.reset(freq=10000000)
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)
 sensor.run(1)
+sensor.skip_frames(time=10000)
 distance = 17
 starting_xposition = 20
-
 pixels_threshold = 50
-green_threshold = (60, 100, -128, -20, -128, 127)
-yellow_threshold = (60, 100, -20, 127, 19, 127)
-red_threshold =(60, 100, -20, 127, -4, 10)
+green_threshold = (9, 100, -128, -24, 9, 53)
+yellow_threshold = (9, 100, -31, 5, 13, 91)
+red_threshold = (11, 100, -1, 30, -3, 24)
 light_string = "o"*15
 distance_threshold=30
 average_list=[]
+running = True
+
 
 
 def averageLight(average_list):
@@ -33,61 +41,75 @@ def averageLight(average_list):
     print(position_list)
     for average in position_list:
         light_string+=max(average, key=average.count)
-    print(light_string)
+    return light_string
+
+while True:
+    while running:
+        img=sensor.snapshot()
+        img.binary([(35, 100)], invert=True, zero=True)
+        img.top_hat(7, 1)
+        if running:
+            last_light = [0, starting_xposition]
+            light_string = "o"*15
+            light_list = []
+            for light in light_string:
+                light_list.append(light)
+            lights=[]
+            g_blobs = img.find_blobs([green_threshold], pixels_threshold=30)
+            y_blobs = img.find_blobs([yellow_threshold], pixels_threshold=25, area_threshold=40)#, #pixels_threshold=pixels_threshold)
+            r_blobs = img.find_blobs([red_threshold], pixels_threshold=8)#, #pixels_threshold=pixels_threshold)
+            blobs_c = [{'color': 'g', 'blobs': g_blobs}, {'color': 'y', 'blobs': y_blobs}, {'color': 'r', 'blobs':r_blobs}]
+            for blobs in blobs_c:
+                if blobs['blobs']:
+                    color = blobs['color']
+                    if color == 'g':
+                        fill_color = (0,255,0)
+                    elif color == 'y':
+                        fill_color = (255, 255, 0)
+                    else:
+                        fill_color = (255, 0, 0)
+                    for b in blobs['blobs']:
+                        lights.append([b, color])
+                        rect = b.rect()
+                        tmp=img.draw_rectangle(b[0:4], color=fill_color, thickness=10)
+            if (lights and len(lights)<=15):
+                light_string = ""
+                while lights:
+                    min_x_light = lights[0]
+                    for light in lights:
+                        if light[0].x() < min_x_light[0].x():
+                            min_x_light = light
+                    if min_x_light[0].x()-distance_threshold >= last_light[1]:
+                        last_light[1] += 20
+                        if last_light[0] < 15:
+                            light_list[last_light[0]] = "o"
+                    else:
+                        if last_light[0]<15:
+                            light_list[last_light[0]] = min_x_light[1]
+                        lights.remove(min_x_light)
+                        last_light[1] = min_x_light[0].x()
+                    last_light[0] += 1
+                for light in light_list:
+                    light_string += light
+            average_list.append(light_string)
+            if len(average_list) == 10:
+                running = False
 
 
+                print("Average: ", end = "")
+                light_string = averageLight(average_list)
+                average_list.clear()
+                light_string_list = [char for char in light_string]
+                light_string = ",".join(light_string_list)+"\n"
+                uart_A.write(light_string)
+                print(light_string)
+
+    while not running:
+        data = uart_A.read()
+        if data:
+            running = True
 
 
 while True:
-    last_light=[0, starting_xposition]
-    light_string = "o"*15
-    light_list = []
-    for light in light_string:
-        light_list.append(light)
-    lights=[]
     img=sensor.snapshot()
-    blobs = img.find_blobs([green_threshold], area_threshold=200, pixels_threshold=pixels_threshold, merge=False)
-    if blobs:
-        for b in blobs:
-           tmp=img.draw_rectangle(b[0:4], color=(0,255,0), thickness=3)
-           lights.append([b, "g"])
-    blobs = img.find_blobs([yellow_threshold], area_threshold=200, pixels_threshold=pixels_threshold, merge=False)
-    if blobs:
-        for b in blobs:
-            tmp=img.draw_rectangle(b[0:4], color=(255,255,0), thickness=3)
-            lights.append([b, "y"])
-    blobs = img.find_blobs([red_threshold], area_threshold=200, pixels_threshold=pixels_threshold, merge=False)
-    if blobs:
-        for b in blobs:
-            tmp=img.draw_rectangle(b[0:4], color=(255,0,0), thickness=3)
-            lights.append([b, "r"])
-    if (lights and len(lights)<=15):
-        light_string=""
-
-        while lights:
-            min_x_light=lights[0]
-            for light in lights:
-                if light[0].x()<min_x_light[0].x():
-                    min_x_light=light
-            if min_x_light[0].x()-distance_threshold >= last_light[1]:
-                last_light[1]+=20
-                if last_light[0]<15:
-                    light_list[last_light[0]]="o"
-            else:
-                if last_light[0]<15:
-                    light_list[last_light[0]]=min_x_light[1]
-                lights.remove(min_x_light)
-                last_light[1]=min_x_light[0].x()
-            last_light[0]+=1
-        for light in light_list:
-            light_string+=light
-    average_list.append(light_string)
-    if len(average_list)==5:
-        print("Average: ", end="")
-        averageLight(average_list)
-        average_list.clear()
-    print(light_string)
-
-
-
     lcd.display(img)
