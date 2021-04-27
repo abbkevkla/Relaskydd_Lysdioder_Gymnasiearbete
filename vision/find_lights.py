cb6f67c09e0573c40c3ed4e530cb3a38e63c0f3f
Nils Borg <Nils.Borg@abbindustrigymnasium.se>
2021-04-27 09:03:26 +0200

    comments

--- vision/find_lights.py
+++ vision/find_lights.py
@@ -33,7 +33,7 @@ distance_threshold=30
 average_list=[]
 running = False
 
-def averageLight(average_list):
+def averageLight(average_list):  #find average for each position in list of results
     position_list=[""]*15
     light_string=""
     for lights in average_list:
@@ -50,7 +50,7 @@ while True:
     while running:
         img=sensor.snapshot()
         img.binary([(35, 100)], invert=True, zero=True)
-        img.top_hat(7, 1)
+        img.top_hat(7, 1) #extracts small elements giving clearer edges to objects
         if running:
             last_light = [0, starting_xposition]
             light_string = "o"*15
@@ -61,7 +61,7 @@ while True:
             g_blobs = img.find_blobs([green_threshold], pixels_threshold=30)
             y_blobs = img.find_blobs([yellow_threshold], pixels_threshold=10, area_threshold=10)#, #pixels_threshold=pixels_threshold)
             r_blobs = img.find_blobs([red_threshold], pixels_threshold=8)#, #pixels_threshold=pixels_threshold)
-            blobs_c = [{'color': 'g', 'blobs': g_blobs}, {'color': 'y', 'blobs': y_blobs}, {'color': 'r', 'blobs':r_blobs}]
+            blobs_c = [{'color': 'g', 'blobs': g_blobs}, {'color': 'y', 'blobs': y_blobs}, {'color': 'r', 'blobs':r_blobs}] #object with color blobs
             for blobs in blobs_c:
                 if blobs['blobs']:
                     color = blobs['color']
@@ -72,30 +72,30 @@ while True:
                     else:
                         fill_color = (255, 0, 0)
                     for b in blobs['blobs']:
-                        lights.append([b, color])
+                        lights.append([b, color]) #add blobs to list
                         rect = b.rect()
                         #tmp=img.draw_rectangle(b[0:4], color=fill_color, thickness=10)
-            if (lights and len(lights)<=15):
+            if (lights and len(lights)<=15): #check that there are not too many elements
                 light_string = ""
                 while lights:
                     min_x_light = lights[0]
                     for light in lights:
                         if light[0].x() < min_x_light[0].x():
                             min_x_light = light
-                    if min_x_light[0].x()-distance_threshold >= last_light[1]:
-                        last_light[1] += 20
+                    if min_x_light[0].x()-distance_threshold >= last_light[1]: #if next blob is out of range
+                        last_light[1] += 20 #add 20 to start of searching area
                         if last_light[0] < 15:
-                            light_list[last_light[0]] = "o"
+                            light_list[last_light[0]] = "o" #empty light
                     else:
-                        if last_light[0]<15:
-                            light_list[last_light[0]] = min_x_light[1]
+                        if last_light[0]<15:  #if there's a light in the first position
+                            light_list[last_light[0]] = min_x_light[1] #minimum position is set to its x value
                         lights.remove(min_x_light)
                         last_light[1] = min_x_light[0].x()
                     last_light[0] += 1
                 for light in light_list:
                     light_string += light
             average_list.append(light_string)
-            if len(average_list) == 10:
+            if len(average_list) == 10: #when 10 recordings hade been made, format and send string
                 print(average_list)
                 running = False
                 print("Average: ", end = "")
